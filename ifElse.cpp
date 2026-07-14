1ee1ec4bff3f095e6bd78535acb3a98282916a2f
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-14 10:24:42 +0700

    kondisi if else untuk cek status dengan parameter

--- ifElse.cpp
+++ ifElse.cpp
@@ -5,3 +5,11 @@ float HitungRerataBerparameter(float x, float y) {
     return (x + y) / 2;
 }
 
+string CekStatusBerparameter(float a){
+    if (a >= 60){
+        return "Lulus";
+    }
+    else{
+        return "Tidak Lulus";
+    }
+}
